04c31633b81e19d8a5abf2db66abbc0425626f57
rhaynes3k <rhaynes3k@gmail.com>
2021-05-31 00:42:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,24 +3,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   let tForm = document.getElementById('create-task-form')
   let tList = document.getElementById('tasks')
-  let btn = document.createElement('BUTTON')
-  btn.innerHTML = 'Delete'
   tForm.addEventListener("submit", function(e){
     e.preventDefault()
-  let input = document.getElementById('new-task-description').value
-  tList.innerHTML += `<li>
-    ${input}
-    <button> Delete </button>
-    </li>
-    `
-  tForm.reset()
-  //  start of delete function
-  function delBtn(e){
-    e.target.parentElement.remove()
-  }
+    let input = document.getElementById('new-task-description').value
+    tList.innerHTML +=
+      `
+        <li>
+          ${input}
+          <button> Delete </button>
+        </li>
+      `
+    tForm.reset()
+    //  start of delete function
+    function delBtn(e){
+      e.target.parentElement.remove()
+    }
+    // end delete function
   tList.addEventListener('click', delBtn)
   })
-  // end delete function
-
-  // your code here
 });
